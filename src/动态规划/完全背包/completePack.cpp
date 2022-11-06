5cc2e2d8a5e68b59602847b264f5e5ff12cfe1fd
superrabbit11223344 <gongwei808@hotmail.com>
2022-11-06 22:53:34 +0800

    .gitignore修改，本地环境配置，完全背包问题

---
+++ src/动态规划/完全背包/completePack.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 先遍历物品，在遍历背包
+void test_CompletePack_1() {
+    vector<int> weight = {1, 3, 4};
+    vector<int> value = {15, 20, 30};
+    int bagWeight = 4;
+    vector<int> dp(bagWeight + 1, 0);
+    for(int i = 0; i < weight.size(); i++) { // 遍历物品
+        for(int j = weight[i]; j <= bagWeight; j++) { // 遍历背包容量
+            dp[j] = max(dp[j], dp[j - weight[i]] + value[i]);
+        }
+    }
+    cout << dp[bagWeight] << endl;
+}
+
+// 先遍历背包，再遍历物品
+void test_CompletePack_2() {
+    vector<int> weight = {1, 3, 4};
+    vector<int> value = {15, 20, 30};
+    int bagWeight = 4;
+
+    vector<int> dp(bagWeight + 1, 0);
+
+    for(int j = 0; j <= bagWeight; j++) { // 遍历背包容量
+        for(int i = 0; i < weight.size(); i++) { // 遍历物品
+            if (j - weight[i] >= 0) dp[j] = max(dp[j], dp[j - weight[i]] + value[i]);
+        }
+    }
+    cout << dp[bagWeight] << endl;
+}
+
+int main() {
+    test_CompletePack_1();
+    // test_CompletePack_2();
+}
