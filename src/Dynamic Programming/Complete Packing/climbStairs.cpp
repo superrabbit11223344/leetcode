a3835c88a5517a0581d3ef63ab9595ca76d8f26f
superrabbit11223344 <gongwei808@hotmail.com>
2022-11-08 18:08:03 +0800

    爬楼梯问题 完全背包问题

---
+++ src/Dynamic Programming/Complete Packing/climbStairs.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int climbStairs(int n, int m) {
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++) {  // 先遍历背包
+        for (int j = 1; j <= m; j++) {  // 再遍历物品
+            if (i - j >= 0) dp[i] += dp[i - j];
+        }
+    }
+    return dp[n];
+}
+
+int main() {
+    int result = climbStairs(3, 2); // 需要n阶才能到达楼顶，一次可以爬1, 2, ..., m层台阶
+    cout << result << endl;
+}
