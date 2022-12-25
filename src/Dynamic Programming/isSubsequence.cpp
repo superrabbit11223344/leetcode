f0c5cb4ae24f4d4206aba2cd2c9810a873099bee
superrabbit11223344 <gongwei808@hotmail.com>
2022-12-25 17:16:37 +0800

    update

--- src/Dynamic Programming/isSubsequence.cpp
+++ src/Dynamic Programming/isSubsequence.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// 判断子序列
 bool isSubsequence(string s, string t)
 {
     vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
@@ -13,8 +14,8 @@ bool isSubsequence(string s, string t)
             if (s[i - 1] == t[j - 1])
                 dp[i][j] = dp[i - 1][j - 1] + 1;
             else
-                // dp[i][j] = dp[i][j - 1];
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                dp[i][j] = dp[i][j - 1];
+                // dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
         }
     }
     if (dp[s.size()][t.size()] == s.size())
